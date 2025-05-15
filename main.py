160b11a155f66f329b7bbab6683a7926c817ed49
muhammadhammad2005 <youyt.manager@gmail.com>
2025-05-15 19:12:50 +0500

    Fix: added missing dependency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from app import views
+import os
 
 app = Flask(__name__) # webserver gateway interphase (WSGI)
 
@@ -12,3 +13,6 @@ app.add_url_rule(rule='/app/gender/',
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)

abcd0b7104899aa59de916f3f9d4b267e4801c89
muhammadhammad2005 <youyt.manager@gmail.com>
2025-05-16 12:07:51 +0500

    first

--- main.py
+++ main.py
@@ -13,6 +13,5 @@ app.add_url_rule(rule='/app/gender/',
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
